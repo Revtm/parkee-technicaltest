08d86ba49111181edd1585df16baf6e5d4c6819f
Revtm <kykylizaro98@gmail.com>
2024-06-14 15:52:32 +0700

    init Node and SingleLinkedList

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        singleLinkedList.insertAtEnd(5);
+        singleLinkedList.display();
+    }
+}
---
+++ src/SingleLinkedList.java
@@ -0,0 +1,43 @@
+public class SingleLinkedList {
+    private Node head = null;
+
+    public void insertAtEnd(int data){
+        if(head == null){
+            head = new Node(data);
+        } else {
+            head = insertToLastNode(data, head);
+        }
+    }
+
+    private Node insertToLastNode(int data, Node node){
+        if(node.getNext() != null){
+            return insertToLastNode(data, node.getNext());
+        }else{
+            Node newNode = new Node(data);
+            node.setNext(newNode);
+            return node;
+        }
+    }
+
+    public void display(){
+        if(head == null){
+            System.out.println("[]");
+        }else{
+            System.out.print("[");
+            display(head);
+            System.out.println("]");
+        }
+    }
+
+    private void display(Node node){
+        if(node == null){
+            System.out.print("");
+        }else{
+            System.out.print(node.getData());
+            if(node.getNext() != null){
+                System.out.print(",");
+            }
+            display(node.getNext());
+        }
+    }
+}
