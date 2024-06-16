202f2ddcc35d2935884a3f6d49228929679971e5
Revtm <kykylizaro98@gmail.com>
2024-06-16 23:00:14 +0700

    move config value to properties file

--- ParkingPOS/src/main/java/com/example/parkingpos/configuration/DatabaseConfig.java
+++ ParkingPOS/src/main/java/com/example/parkingpos/configuration/DatabaseConfig.java
@@ -1,22 +1,32 @@
 package com.example.parkingpos.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.DataSourceFactory;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+    @Value("${db.postgres.driver}")
+    private String driver;
+    @Value("${db.postgres.url}")
+    private String url;
+    @Value("${db.postgres.username}")
+    private String userName;
+    @Value("${db.postgres.password}")
+    private String password;
+
     @Bean
     public DataSource getPostgreDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/parking-pos");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("pergipergi");
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
